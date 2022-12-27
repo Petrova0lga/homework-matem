613712753dc01253beed3b5bde1263d08976ef1c
olga petrova <petrovaolga629@gmail.com>
2022-12-27 21:33:20 +0200

    final_version

--- 12.12.22 dz/expression_tester.py
+++ 12.12.22 dz/expression_tester.py
@@ -1,18 +1,85 @@
 import random
 
-def is_exit_loop(tase):
-    if tase == "q":
-        exit(0)
+def vastus_kontroll(kasutaja_vastus, real_vastus, tase):
+    if kasutaja_vastus == real_vastus:
+        print("Õige vastus")
+        return True
 
+    print("Vale vastus")
+    return False
+    
+
+def rakenda_math(operators, val1, val2, tase):
+    oiged_vastused = []
+    operators = operators.split(",")
+    operator_pikkus = len(operators)
+
+    for i in range(0, operator_pikkus):
+        if operators[i] == "+":
+            kusimus = "{val1} + {val2} = ".format(val1 = val1, val2 = val2)
+            vastus = input(kusimus)
+            vastus = float(vastus)
+            real_vastus = val1 + val2
+
+            oige_vastus = vastus_kontroll(kasutaja_vastus=vastus, real_vastus=real_vastus, tase=tase)
+            oiged_vastused.append(oige_vastus)
+            continue
+
+        if operators[i] == "-":
+            kusimus = "{val1} - {val2} = ".format(val1 = val1, val2 = val2)
+            vastus = input(kusimus)
+            vastus = float(vastus)
+            real_vastus = val1 - val2
+
+            oige_vastus = vastus_kontroll(kasutaja_vastus=vastus, real_vastus=real_vastus, tase=tase)
+            oiged_vastused.append(oige_vastus)
+            continue
+
+        if operators[i] == "*":
+            kusimus = "{val1} * {val2} = ".format(val1 = val1, val2 = val2)
+            vastus = input(kusimus)
+            vastus = float(vastus)
+            real_vastus = val1 * val2
+
+            oige_vastus = vastus_kontroll(kasutaja_vastus=vastus, real_vastus=real_vastus, tase=tase)
+            oiged_vastused.append(oige_vastus)
+            continue
+
+        if operators[i] == "/":
+            kusimus = "{val1} / {val2} = ".format(val1 = val1, val2 = val2)
+            vastus = input(kusimus)
+            vastus = float(vastus)
+            real_vastus = val1 / val2
+
+            oige_vastus = vastus_kontroll(kasutaja_vastus=vastus, real_vastus=real_vastus, tase=tase)
+            oiged_vastused.append(oige_vastus)
+            continue
+
+        if operators[i] == "**":
+            kusimus = "{val1} ** {val2} = ".format(val1 = val1, val2 = val2)
+            vastus = input(kusimus)
+            vastus = float(vastus)
+            real_vastus = val1 ** val2
+
+            oige_vastus = vastus_kontroll(kasutaja_vastus=vastus, real_vastus=real_vastus, tase=tase)
+            oiged_vastused.append(oige_vastus)
+            continue
+
+        print("vale operator: {op}".format(op=operators[i]))
+        exit(0)
+    
+    return oiged_vastused
+        
 while True:
     tase = input("(Sisesta q väljumiseks) Sisesta tase: tase1, tase2, tase3: ")
-    is_exit_loop(tase)
-
+    if tase == "q":
+        exit(0)
     
     if tase == "tase1" or tase == "tase2" or tase == "tase3":
         break
 
     print("vale tase, sisesta: tase1, tase2, tase3")
+
 
 teg_num = input("(Sisesta q väljumiseks) Sisesta tegevuse number, näiteks +,-,/,*,**: ")
 juh_num = input("(Sisesta q väljumiseks) Sisesta juhuslik number: ")
@@ -20,8 +87,23 @@ juh_num = int(juh_num)
 
 juh_num1 = random.randint(0, juh_num)
 juh_num2 = random.randint(0, juh_num)
-mat_ops = teg_num.split(",")
-print(mat_ops)
+
+oiged_vastused = rakenda_math(teg_num, juh_num1, juh_num2, tase)
+hinne = (oiged_vastused.count(True) / len(oiged_vastused)) * 100
+
+print("tulemus: {tul}".format(tul=hinne))
+
+if hinne < 60:
+    print("hinne on 2")
+
+if hinne >= 60 and hinne < 75:
+    print("hinne on 3")
+
+if hinne >= 75 and hinne <= 90:
+    print("hinne: 4")
+
+if hinne > 90:
+    print("hinne on 5")
 
 
 
