ac760c0529ea805e3a200d96af28f5fa8384a4ba
olga petrova <petrovaolga629@gmail.com>
2022-12-27 20:10:22 +0200

    read values

--- 12.12.22 dz/expression_tester.py
+++ 12.12.22 dz/expression_tester.py
@@ -1,3 +1,4 @@
+import random
 
 def is_exit_loop(tase):
     if tase == "q":
@@ -12,6 +13,19 @@ while True:
         break
 
     print("vale tase, sisesta: tase1, tase2, tase3")
+
+teg_num = input("(Sisesta q väljumiseks) Sisesta tegevuse number, näiteks +,-,/,*,**: ")
+juh_num = input("(Sisesta q väljumiseks) Sisesta juhuslik number: ")
+juh_num = int(juh_num)
+
+juh_num1 = random.randint(0, juh_num)
+juh_num2 = random.randint(0, juh_num)
+mat_ops = teg_num.split(",")
+print(mat_ops)
+
+
+
+
 
 
 
